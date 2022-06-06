053e8aade2358be131e2d70a72b8a171c2cf02c9
Riku Rainio <rkurainio@gmail.com>
2022-06-06 14:41:50 +0300

    change title text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             </div>
             <div className='mx-5'>
                 <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-                    darkmode toggler
+                    darkmode
                 </button>
             </div>
         </div>
