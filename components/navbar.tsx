b0db71f6e8669ddb35ea2c5fff56fecb73d9385c
Riku Rainio <rkurainio@gmail.com>
2022-06-14 16:40:52 +0300

    remove icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,17 +2,23 @@ import React, { useEffect, useContext } from 'react'
 import {useTheme} from 'next-themes'
 import { GlobalCtx } from './globalContext'
 import { BiChevronLeft } from 'react-icons/bi'
+import { CgZoomIn } from 'react-icons/cg'
 
 const Navbar = () => {
-    useEffect(() => {
-        setTheme('dark')
-    }, [])
-
     const {theme, setTheme} = useTheme()
     const gc = useContext(GlobalCtx)!
 
+    useEffect(() => {
+        setTheme('dark')
+    }, [setTheme])
+
+    const handleClickZoom = () => {
+        gc.setZoom(gc.zoom+.1)
+      }
+    
     return (
         <div className="
+            overflow-hidden
             flex justify-center
             h-full
             top-0
@@ -22,17 +28,35 @@ const Navbar = () => {
           >
             <div>
                 <BiChevronLeft
-                onClick={() => { gc.setHideDraftTab(!gc.hideDraftTab) }}
-                className={`
+                    onClick={() => { gc.setHideDraftTab(!gc.hideDraftTab) }}
+                    className={`
+                        ml-1
+                        mt-10
+                        text-5xl
+                        ${gc.hideDraftTab && 'rotate-180'}
+                        cursor-pointer
+                        text-zinc-400
+                        hover:contrast-200
+                        hover:brightness-200
+                    `}
+                >
+                </BiChevronLeft>
+                {/*
+                <CgZoomIn
+                    onClick={() => {handleClickZoom()}}
+                    className={`
                     text-5xl
-                    ${gc.hideDraftTab && 'rotate-180'}
+                    mt-1
+                    ml-1
                     cursor-pointer
                     text-zinc-400
                     hover:contrast-200
                     hover:brightness-200
                 `}
                 >
-                </BiChevronLeft>
+                    
+                </CgZoomIn>
+                */}
           </div>
         </div>
     )
