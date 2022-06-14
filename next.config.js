40e84b1daf5c9cf34eaff6170853966546e162b8
Riku Rainio <rkurainio@gmail.com>
2022-06-14 16:40:41 +0300

    test raw mode

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['ddragon.leagueoflegends.com', 'upload.wikimedia.org']
-  }
+  },
+  experimental: { images: { layoutRaw: true } }
 }
 
 module.exports = nextConfig
